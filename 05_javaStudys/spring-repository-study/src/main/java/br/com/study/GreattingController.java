25c009a1dd1b7f4db4ead96a548f60c29b326bfb
falcao00 <nickbartowskireserva@gmail.com>
2022-12-01 02:03:55 -0300

    SPRB-02 handle, exception e algumas coisas a mais

--- 05_javaStudys/spring-repository-study/src/main/java/br/com/study/GreattingController.java
+++ 05_javaStudys/spring-repository-study/src/main/java/br/com/study/GreattingController.java
@@ -2,9 +2,13 @@ package br.com.study;
 
 import java.util.concurrent.atomic.AtomicLong;
 
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import br.com.study.exception.UnsuportedMathOperation;
 
 @RestController
 public class GreattingController {
@@ -13,8 +17,31 @@ public class GreattingController {
 	private static final String template = "Hello, %s!";
 	private final AtomicLong count = new AtomicLong();
 	
-	@RequestMapping("/grettings")
-	public Grettings gretting(@RequestParam(value = "name", defaultValue = "world") String name) {
-		return new Grettings(count.incrementAndGet(), String.format(template, name));
+	@RequestMapping(value = "/sum/{numberOne}/{numberTwo}", 
+			method=RequestMethod.GET)
+	public Double sum(
+			@PathVariable(value = "numberOne") String numberOne,
+			@PathVariable(value = "numberTwo") String numberTwo) throws Exception {
+				
+		if( !isNumeric(numberOne) || !isNumeric(numberTwo) ) {
+			throw new UnsuportedMathOperation("Please Set a numeric value");
+		} 
+		
+		
+		return convertToDouble(numberOne) + convertToDouble(numberTwo);
+		
+	}
+
+	private double convertToDouble(String strNumber) {
+		if(strNumber == null) return 0D;
+		String number = strNumber.replaceAll(",", ".");
+		if(isNumeric(number)) return Double.parseDouble(number);
+		return 0D;
+	}
+
+	private boolean isNumeric(String strNumber) {
+		if(strNumber == null) return false;
+		String number = strNumber.replaceAll(",", ".");
+		return number.matches("[-+]?[0-9]*\\.?[0-9]+");
 	}
 }
